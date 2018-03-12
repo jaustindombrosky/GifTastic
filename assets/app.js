4b5310f0e1cdbbc8375715da5249fd08d789b5d6
jaustindombrosky <austindombrosky@gmail.com>
2018-03-12 15:44:09 -0600

    update before work

--- assets/app.js
+++ assets/app.js
@@ -1,30 +1,42 @@
 $(document).ready(function() {
 
-var gifRocks = {
-    rocksMinerals: ["", "", "", "", "", "", "", "", ""],
+var gifDinosaurs = {
+    typeDinosaurs: ["Tyrannosaurus Rex", "Triceratops", "Velociraptor", "Stegosaurus", "Allosaurus", "Spinosaurus", "Apatosaurus", "Brachiosaurus", "Archaeopteryx", "Dilophosaurus", "Iguanodon"],
     firstPick: "",
     giphyApiUrl: "",
     giphyApiKey: "",
     renderButtons: function() {
         $("#buttonsBar").empty();
-		for (var i = 0; i < this.rocksMinerals.length; i++) {
+		for (var i = 0; i < this.typeDinosaurs.length; i++) {
 			var a = $("<button>");
 			a.addClass("buttons btn btn-danger");
-			a.attr("data-name", this.rocksMinerals[i]);
-			a.text(this.rocksMinerals[i]);
+			a.attr("data-name", this.typeDinosaurs[i]);
+			a.text(this.typeDinosaurs[i]);
             $("#buttonsBar").append(a);
         }
         $(".buttons").on("click", function() {
-            gifRocksObj.firstPick = $(this).attr("data-name");
-            gifRocksObj.displayGifs();
+            gifDinosaursObj.firstPick = $(this).attr("data-name");
+            gifDinosaursObj.displayGifs();
         });
     displayGifs: function() {
-        var rockToShow = this.rocksMinerals;
+        var rockToShow = this.typeDinosaurs;
 		var queryURL = this.giphyApiUrl + "&q=" + tvShowToDisplay + "&limit=12&api_key=" + this.giphyApiKey;
-
+        $.ajax({url: queryURL, method: "GET"}).done(function(response) {
+            $("gifsWindow").empty();
+                for (var i = 0; i < response.data.length; i++){
+                    var showObject = response.data[i];
+                    var showStill = response.data[i].images.fixed_height_small_still.url;
+				    var showMoving = response.data[i].images.fixed_height_small.url;
+				    var showRating = response.data[i].rating;
+                    var showDiv = $("div class='show col-md-4'>");
+                    var pOne = $("<p>").text("Rating: " + showRating);
+                    showDiv.append(pOne);
+                    
+                }
+        }
     }
 $("")
 
 
-    gifRocksObj.renderButtons();
+    gifDinosaursObj.renderButtons();
 });
