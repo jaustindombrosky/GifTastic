247e15b90a6ec426016528437cab4aaa9d83b535
jaustindombrosky <austindombrosky@gmail.com>
2018-03-12 14:38:44 -0600

    newupdate

--- assets/app.js
+++ assets/app.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 var gifRocks = {
     rocksMinerals: ["", "", "", "", "", "", "", "", ""],
     firstPick: "",
-    giphyApiUrl: ,
-    giphyApiKey: ,
+    giphyApiUrl: "",
+    giphyApiKey: "",
     renderButtons: function() {
         $("#buttonsBar").empty();
 		for (var i = 0; i < this.rocksMinerals.length; i++) {
@@ -12,10 +12,19 @@ var gifRocks = {
 			a.addClass("buttons btn btn-danger");
 			a.attr("data-name", this.rocksMinerals[i]);
 			a.text(this.rocksMinerals[i]);
-			$("#buttonsBar").append(a);
+            $("#buttonsBar").append(a);
+        }
+        $(".buttons").on("click", function() {
+            gifRocksObj.firstPick = $(this).attr("data-name");
+            gifRocksObj.displayGifs();
+        });
+    displayGifs: function() {
+        var rockToShow = this.rocksMinerals;
+		var queryURL = this.giphyApiUrl + "&q=" + tvShowToDisplay + "&limit=12&api_key=" + this.giphyApiKey;
 
     }
-}
+$("")
 
 
-}
+    gifRocksObj.renderButtons();
+});
